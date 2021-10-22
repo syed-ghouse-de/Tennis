a8f1045822e6635cfefb613d31911139f8fbf47e
syed.ghouse <syed_g@yahoo.com>
2021-10-22 09:04:54 +0200

    Framework exception class added as part of framework

---
+++ Source/Hexagon.Game.Framework/Exceptions/BaseException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hexagon.Game.Framework.Exceptions
+{
+    /// <summary>
+    /// Framework abstract base exception class
+    /// </summary>
+    public abstract class BaseException : Exception
+    {
+        /// <summary>
+        /// Default exception constructor
+        /// </summary>
+        public BaseException() { }
+
+        /// <summary>
+        /// Constructor which passes error message
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        public BaseException(string message) : base(message) { }
+    }
+}
---
+++ Source/Hexagon.Game.Framework/Exceptions/DuplicateException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hexagon.Game.Framework.Exceptions
+{
+    /// <summary>
+    /// Exception class for duplicate
+    /// </summary>
+    public class DuplicateException : BaseException
+    {
+        /// <summary>
+        /// Default exception constructor
+        /// </summary>
+        public DuplicateException()
+        { }
+
+        /// <summary>
+        /// Constructor which passes error message
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        public DuplicateException(string message) : base(message) { }
+    }
+}

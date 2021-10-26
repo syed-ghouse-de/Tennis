0e9786fc1ff697342fdda82d7ff38ae4e3b88686
syed.ghouse <syed_g@yahoo.com>
2021-10-26 08:42:59 +0200

    Added 10 test cases for player games out of which 6  test cases are passed and 4 test cases are failed

--- Source/Hexagon.Game.Tennis.Test/Player/PlayerGameTest.cs
+++ Source/Hexagon.Game.Tennis.Test/Player/PlayerGameTest.cs
@@ -15,7 +15,7 @@ namespace Hexagon.Game.Tennis.Test
     public class PlayerGameTest : BaseTest
     {
         [Fact]
-        public void PlayerGameWithFirstInprogressGame()
+        public void PlayGameWithFirstInprogressGame()
         {
             IMatch match = new Match();
             match.Players = AddPlayers();
@@ -70,5 +70,286 @@ namespace Hexagon.Game.Tennis.Test
                  match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
                     p => p.Player.Id.Equals(firstPlayer.Opponent.Identity.Id)).Select(s => s.Point.ToString())));
         }
+
+        [Fact]
+        public void PlayGameWithPlayerPoints()
+        {
+            IMatch match = new Match();
+            match.Players = AddPlayers();
+
+            var firstPlayer = match.Players.FirstPlayer;
+            var secondPlayer = match.Players.SecondPlayer;
+            match.Players.Server = firstPlayer;
+            match.Start();
+
+            firstPlayer.Win();          // 15   0
+            secondPlayer.Win();         // 15   15
+            firstPlayer.Win();          // 30   15
+            firstPlayer.Win();          // 40   15
+            secondPlayer.Win();         // 40   30  
+
+            Assert.Equal("Fifteen-Fifteen-Thirty-Forty-Forty", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Identity.Id)).Select(s => s.Point.ToString())));
+            Assert.Equal("Love-Fifteen-Fifteen-Fifteen-Thirty", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Opponent.Identity.Id)).Select(s => s.Point.ToString())));
+        }
+
+        [Fact]
+        public void PlayGameWithFirstPlayerGamePoint()
+        {
+            IMatch match = new Match();
+            match.Players = AddPlayers();
+
+            var firstPlayer = match.Players.FirstPlayer;
+            var secondPlayer = match.Players.SecondPlayer;
+            match.Players.Server = firstPlayer;
+            match.Start();
+
+            firstPlayer.Win();          // 15   0
+            secondPlayer.Win();         // 15   15
+            firstPlayer.Win();          // 30   15
+            firstPlayer.Win();          // 40   15
+            secondPlayer.Win();         // 40   30  
+            firstPlayer.Win();          // Game 30
+
+            Assert.Equal("Fifteen-Fifteen-Thirty-Forty-Forty-GamePoint", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Identity.Id)).Select(s => s.Point.ToString())));
+            Assert.Equal("Love-Fifteen-Fifteen-Fifteen-Thirty-Thirty", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Opponent.Identity.Id)).Select(s => s.Point.ToString())));
+        }
+
+        [Fact]
+        public void PlayGameWithFirstPlayerStraightGamePointWin()
+        {
+            IMatch match = new Match();
+            match.Players = AddPlayers();
+
+            var firstPlayer = match.Players.FirstPlayer;
+            var secondPlayer = match.Players.SecondPlayer;
+            match.Players.Server = firstPlayer;
+            match.Start();
+
+            firstPlayer.Win();          // 15   0
+            firstPlayer.Win();          // 30   0
+            firstPlayer.Win();          // 40   0    
+            firstPlayer.Win();          // Game 0
+
+            Assert.Equal("Fifteen-Thirty-Forty-GamePoint", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Identity.Id)).Select(s => s.Point.ToString())));
+            Assert.Equal("Love-Love-Love-Love", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Opponent.Identity.Id)).Select(s => s.Point.ToString())));
+        }
+
+        [Fact]
+        public void PlayGameWithSecondPlayerStraightGamePointWin()
+        {
+            IMatch match = new Match();
+            match.Players = AddPlayers();
+
+            var firstPlayer = match.Players.FirstPlayer;
+            var secondPlayer = match.Players.SecondPlayer;
+            match.Players.Server = firstPlayer;
+            match.Start();
+
+            secondPlayer.Win();          // 0   15
+            secondPlayer.Win();          // 0   30
+            secondPlayer.Win();          // 0   40    
+            secondPlayer.Win();          // 0   Game
+
+            Assert.Equal("Love-Love-Love-Love", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Identity.Id)).Select(s => s.Point.ToString())));
+            Assert.Equal("Fifteen-Thirty-Forty-GamePoint", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Opponent.Identity.Id)).Select(s => s.Point.ToString())));
+        }
+
+        [Fact]
+        public void PlayGameWithDeuce()
+        {
+            IMatch match = new Match();
+            match.Players = AddPlayers();
+
+            var firstPlayer = match.Players.FirstPlayer;
+            var secondPlayer = match.Players.SecondPlayer;
+            match.Players.Server = firstPlayer;
+            match.Start();
+
+            firstPlayer.Win();          // 15   0
+            secondPlayer.Win();         // 15   15
+            firstPlayer.Win();          // 30   15
+            firstPlayer.Win();          // 40   15
+            secondPlayer.Win();         // 40   30
+            secondPlayer.Win();         // 40   40      Deuce
+
+            Assert.Equal("Fifteen-Fifteen-Thirty-Forty-Forty-Forty", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Identity.Id)).Select(s => s.Point.ToString())));
+            Assert.Equal("Love-Fifteen-Fifteen-Fifteen-Thirty-Forty", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Opponent.Identity.Id)).Select(s => s.Point.ToString())));
+        }
+
+        [Fact]
+        public void PlayGameWithSecondPlayerAdvantage()
+        {
+            IMatch match = new Match();
+            match.Players = AddPlayers();
+
+            var firstPlayer = match.Players.FirstPlayer;
+            var secondPlayer = match.Players.SecondPlayer;
+            match.Players.Server = firstPlayer;
+            match.Start();
+
+            firstPlayer.Win();          // 15   0
+            secondPlayer.Win();         // 15   15
+            firstPlayer.Win();          // 30   15
+            firstPlayer.Win();          // 40   15
+            secondPlayer.Win();         // 40   30
+            secondPlayer.Win();         // 40   40          Deuce
+            secondPlayer.Win();         // 40   Advantage
+
+            Assert.Equal("Fifteen-Fifteen-Thirty-Forty-Forty-Forty-Forty", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Identity.Id)).Select(s => s.Point.ToString())));
+            Assert.Equal("Love-Fifteen-Fifteen-Fifteen-Thirty-Forty-Advantage", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Opponent.Identity.Id)).Select(s => s.Point.ToString())));
+        }
+
+        [Fact]
+        public void PlayGameWithFirstPlayerAdvantage()
+        {
+            IMatch match = new Match();
+            match.Players = AddPlayers();
+
+            var firstPlayer = match.Players.FirstPlayer;
+            var secondPlayer = match.Players.SecondPlayer;
+            match.Players.Server = firstPlayer;
+            match.Start();
+
+            firstPlayer.Win();          // 15            0
+            secondPlayer.Win();         // 15           15
+            firstPlayer.Win();          // 30           15
+            firstPlayer.Win();          // 40           15
+            secondPlayer.Win();         // 40           30
+            secondPlayer.Win();         // 40           40          Deuce            
+            firstPlayer.Win();          // Advantage    40
+
+            Assert.Equal("Fifteen-Fifteen-Thirty-Forty-Forty-Forty-Advantage", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Identity.Id)).Select(s => s.Point.ToString())));
+            Assert.Equal("Love-Fifteen-Fifteen-Fifteen-Thirty-Forty-Forty", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Opponent.Identity.Id)).Select(s => s.Point.ToString())));
+        }
+
+        [Fact]
+        public void PlayGameWithMultipleDeuceAndAdvantages()
+        {
+            IMatch match = new Match();
+            match.Players = AddPlayers();
+
+            var firstPlayer = match.Players.FirstPlayer;
+            var secondPlayer = match.Players.SecondPlayer;
+            match.Players.Server = firstPlayer;
+            match.Start();
+
+            firstPlayer.Win();          // 15            0
+            secondPlayer.Win();         // 15           15
+            firstPlayer.Win();          // 30           15
+            firstPlayer.Win();          // 40           15
+            secondPlayer.Win();         // 40           30
+            secondPlayer.Win();         // 40           40      Deuce            
+            firstPlayer.Win();          // Advantage    40
+            secondPlayer.Win();         // 40           40      Deuce
+            secondPlayer.Win();         // 40           40
+            firstPlayer.Win();          // 40           40      Deuce
+            firstPlayer.Win();          // Advantage    40
+            secondPlayer.Win();         // 40           40
+
+            Assert.Equal("Fifteen-Fifteen-Thirty-Forty-Forty-Forty-Advantage-Forty-Forty-Forty-Advantage-Forty", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Identity.Id)).Select(s => s.Point.ToString())));
+            Assert.Equal("Love-Fifteen-Fifteen-Fifteen-Thirty-Forty-Forty-Forty-Advantage-Forty-Forty-Forty", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Opponent.Identity.Id)).Select(s => s.Point.ToString())));
+        }
+
+        [Fact]
+        public void PlayGameWithMultipleDeuceAndAdvantagesFirstPlayerWin()
+        {
+            IMatch match = new Match();
+            match.Players = AddPlayers();
+
+            var firstPlayer = match.Players.FirstPlayer;
+            var secondPlayer = match.Players.SecondPlayer;
+            match.Players.Server = firstPlayer;
+            match.Start();
+
+            firstPlayer.Win();          // 15            0
+            secondPlayer.Win();         // 15           15
+            firstPlayer.Win();          // 30           15
+            firstPlayer.Win();          // 40           15
+            secondPlayer.Win();         // 40           30
+            secondPlayer.Win();         // 40           40      Deuce            
+            firstPlayer.Win();          // Advantage    40
+            secondPlayer.Win();         // 40           40      Deuce
+            secondPlayer.Win();         // 40           40
+            firstPlayer.Win();          // 40           40      Deuce
+            firstPlayer.Win();          // Advantage    40
+            secondPlayer.Win();         // 40           40
+            firstPlayer.Win();          // Advantage    40
+            firstPlayer.Win();          // GamePoint    40
+
+            Assert.Equal("Fifteen-Fifteen-Thirty-Forty-Forty-Forty-Advantage-Forty-Forty-Forty-Advantage-Forty-Advantage-GamePoint", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Identity.Id)).Select(s => s.Point.ToString())));
+            Assert.Equal("Love-Fifteen-Fifteen-Fifteen-Thirty-Forty-Forty-Forty-Advantage-Forty-Forty-Forty-Forty-Forty", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Opponent.Identity.Id)).Select(s => s.Point.ToString())));
+        }
+
+        [Fact]
+        public void PlayGameWithMultipleDeuceAndAdvantagesSecondPlayerWin()
+        {
+            IMatch match = new Match();
+            match.Players = AddPlayers();
+
+            var firstPlayer = match.Players.FirstPlayer;
+            var secondPlayer = match.Players.SecondPlayer;
+            match.Players.Server = firstPlayer;
+            match.Start();
+
+            firstPlayer.Win();          // 15            0
+            secondPlayer.Win();         // 15           15
+            firstPlayer.Win();          // 30           15
+            firstPlayer.Win();          // 40           15
+            secondPlayer.Win();         // 40           30
+            secondPlayer.Win();         // 40           40      Deuce            
+            firstPlayer.Win();          // Advantage    40
+            secondPlayer.Win();         // 40           40      Deuce
+            secondPlayer.Win();         // 40           40
+            firstPlayer.Win();          // 40           40      Deuce
+            firstPlayer.Win();          // Advantage    40
+            secondPlayer.Win();         // 40           40
+            secondPlayer.Win();         // 40           Advantage
+            secondPlayer.Win();         // 40           GamePoint
+
+            Assert.Equal("Fifteen-Fifteen-Thirty-Forty-Forty-Forty-Advantage-Forty-Forty-Forty-Advantage-Forty-Forty-Forty", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Identity.Id)).Select(s => s.Point.ToString())));
+            Assert.Equal("Love-Fifteen-Fifteen-Fifteen-Thirty-Forty-Forty-Forty-Advantage-Forty-Forty-Forty-Advantage-GamePoint", string.Join("-",
+                 match.Score.GetSet(0).GetGame(0).PlayerPoints.Where(
+                    p => p.Player.Id.Equals(firstPlayer.Opponent.Identity.Id)).Select(s => s.Point.ToString())));
+        }
     }
 }
+
+

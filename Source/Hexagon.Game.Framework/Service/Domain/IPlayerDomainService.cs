525bd94e39a169c74494733c3c3380e814d329f9
syed.ghouse <syed_g@yahoo.com>
2021-11-23 08:01:38 +0100

    Added Player domain service for player business rules

---
+++ Source/Hexagon.Game.Framework/Service/Domain/IPlayerDomainService.cs
@@ -0,0 +1,21 @@
+﻿using Hexagon.Game.Tennis.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hexagon.Game.Framework.Service.Domain
+{
+    /// <summary>
+    /// Interface to manage player service
+    /// </summary>
+    public interface IPlayerDomainService
+    {
+        /// <summary>
+        /// Get all the players
+        /// </summary>
+        /// <returns>Return list of players</returns>
+        List<PlayerEntity> GetPlayers();
+    }
+}
---
+++ Source/Hexagon.Game.Tennis.Domain/Implemenation/PlayerDomainService.cs
@@ -0,0 +1,50 @@
+﻿using Hexagon.Game.Framework.Exceptions;
+using Hexagon.Game.Framework.Service.Domain;
+using Hexagon.Game.Framework.Service.Persistence;
+using Hexagon.Game.Tennis.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hexagon.Game.Tennis.Domain.Implemenation
+{
+    /// <summary>
+    /// Interface to manage player service
+    /// </summary>
+    public class PlayerDomainService : IPlayerDomainService
+    {
+        // Member variable for player persistence service
+        private IPlayerPersistenceService _playerPersistenceService;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public PlayerDomainService(IPlayerPersistenceService playerPersistenceService)
+        {
+            _playerPersistenceService = playerPersistenceService;
+        }
+
+        /// <summary>
+        /// Get all the players
+        /// </summary>
+        /// <returns>Return list of players</returns>
+        public List<PlayerEntity> GetPlayers()
+        {
+            try
+            {
+                // Get all the players from database
+                return _playerPersistenceService.GetPlayers();                    
+            }
+            catch (PersistenceServiceException persistenceException)
+            {
+                throw new DomainServiceException(persistenceException.Message);
+            }
+            catch (Exception exception)
+            {
+                throw new DomainServiceException(exception.Message);
+            }            
+        }
+    }
+}
